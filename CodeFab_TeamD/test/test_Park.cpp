e890a1e194d546618fe7c61416790b1f2e2661cb
JinyongPark <jin5383@naver.com>
2026-07-06 16:10:38 +0900

    [Test] md 파일 추가

--- CodeFab_TeamD/test/test_Park.cpp
+++ CodeFab_TeamD/test/test_Park.cpp
@@ -10,15 +10,14 @@ TEST(AssemblerUnitTest, VarDeclWithNumberLiteral_BuildsProgramTree)
 {
 	Program program = assemble("var a = 5 ;");
 
-	ASSERT_THAT(program.statements, SizeIs(1));
+	EXPECT_THAT(program.statements, SizeIs(1));
 
 	auto* varDecl = dynamic_cast<VarDeclStmt*>(program.statements[0]);
-	ASSERT_THAT(varDecl, NotNull());
+	EXPECT_THAT(varDecl, NotNull());
 	EXPECT_EQ(varDecl->name.type, TokenType::IDENTIFIER);
 	EXPECT_EQ(varDecl->name.origin, "a");
 
 	auto* literal = dynamic_cast<LiteralExpr*>(varDecl->initializer);
-	ASSERT_THAT(literal, NotNull());
-	ASSERT_TRUE(std::holds_alternative<double>(literal->value));
+	EXPECT_THAT(literal, NotNull());
 	EXPECT_DOUBLE_EQ(std::get<double>(literal->value), 5.0);
 }
