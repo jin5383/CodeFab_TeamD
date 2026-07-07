e91fb30cbcb4f3229ef0bad4b2b1a1d21b163914
Hoyoung Lee <hyl4237@gmail.com>
2026-07-07 14:15:34 +0900

    [feature] Checker Unit 자기 자신 참조 초기화 에러 검출 구현
    var 선언의 초기화식이 아직 선언되지 않은(바깥 스코프에도 없는) 자기 자신의
    이름을 참조하면 checkAssembly가 false를 반환하도록 구현한다. BlockStmt/IfStmt/
    ForStmt를 따라가며 스코프 스택을 유지하고, VarDeclStmt에서는 초기화식 평가 시점에
    아직 해당 이름이 스코프에 등록되지 않았음을 이용해 자기 참조를 판별한다.
    SelfReferencingInitializer_ReportsError가 Green이 된다.

--- CodeFab_TeamD/function/Checker_Unit.cpp
+++ CodeFab_TeamD/function/Checker_Unit.cpp
@@ -1,6 +1,97 @@
 #include "../function.h"
+#include <set>
+
+namespace
+{
+	bool exprReferencesName(Expr* expr, const std::string& name)
+	{
+		if (!expr)
+			return false;
+
+		if (auto* variable = dynamic_cast<VariableExpr*>(expr))
+			return variable->name.origin == name;
+
+		if (auto* assign = dynamic_cast<AssignExpr*>(expr))
+			return exprReferencesName(assign->value, name);
+
+		if (auto* binary = dynamic_cast<BinaryExpr*>(expr))
+			return exprReferencesName(binary->left, name) || exprReferencesName(binary->right, name);
+
+		if (auto* unary = dynamic_cast<UnaryExpr*>(expr))
+			return exprReferencesName(unary->right, name);
+
+		if (auto* logical = dynamic_cast<LogicalExpr*>(expr))
+			return exprReferencesName(logical->left, name) || exprReferencesName(logical->right, name);
+
+		if (auto* grouping = dynamic_cast<GroupingExpr*>(expr))
+			return exprReferencesName(grouping->expression, name);
+
+		return false;
+	}
+
+	bool isNameDeclaredInScopes(const std::vector<std::set<std::string>>& scopes, const std::string& name)
+	{
+		for (auto it = scopes.rbegin(); it != scopes.rend(); ++it)
+			if (it->count(name))
+				return true;
+		return false;
+	}
+
+	bool checkStmt(Stmt* stmt, std::vector<std::set<std::string>>& scopes);
+
+	bool checkStmts(const std::vector<Stmt*>& statements, std::vector<std::set<std::string>>& scopes)
+	{
+		for (Stmt* stmt : statements)
+			if (!checkStmt(stmt, scopes))
+				return false;
+		return true;
+	}
+
+	bool checkStmt(Stmt* stmt, std::vector<std::set<std::string>>& scopes)
+	{
+		if (!stmt)
+			return true;
+
+		// var 자신의 초기화식이 같은 이름의 바깥 변수 없이 자기 자신을 참조하면 에러
+		if (auto* varDecl = dynamic_cast<VarDeclStmt*>(stmt))
+		{
+			const std::string& name = varDecl->name.origin;
+			if (varDecl->initializer && exprReferencesName(varDecl->initializer, name) && !isNameDeclaredInScopes(scopes, name))
+				return false;
+
+			scopes.back().insert(name);
+			return true;
+		}
+
+		if (auto* block = dynamic_cast<BlockStmt*>(stmt))
+		{
+			scopes.push_back({});
+			bool ok = checkStmts(block->statements, scopes);
+			scopes.pop_back();
+			return ok;
+		}
+
+		if (auto* ifStmt = dynamic_cast<IfStmt*>(stmt))
+		{
+			if (!checkStmt(ifStmt->thenBranch, scopes))
+				return false;
+			return checkStmt(ifStmt->elseBranch, scopes);
+		}
+
+		if (auto* forStmt = dynamic_cast<ForStmt*>(stmt))
+		{
+			scopes.push_back({});
+			bool ok = checkStmt(forStmt->init, scopes) && checkStmt(forStmt->body, scopes);
+			scopes.pop_back();
+			return ok;
+		}
+
+		return true;
+	}
+}
 
 bool checkAssembly(const Program& program)
 {
-	return true;
+	std::vector<std::set<std::string>> scopes(1);
+	return checkStmts(program.statements, scopes);
 }
