1cd4e2b99fddc34921557783ad3204cfab3cfc43
thelogic0 <thelogic@gmail.com>
2026-07-07 10:16:55 +0900

    [feature] 가독성 개선(3항 연산 -> if문)

--- CodeFab_TeamD/function/Assembler_Token_Unit.cpp
+++ CodeFab_TeamD/function/Assembler_Token_Unit.cpp
@@ -35,39 +35,42 @@ std::vector<Token> tokenizeSource(const std::string& source)
 	{
 		char c = source[i];
 
-			// 1단계: 공백은 토큰 없이 SKIP
-			if (std::isspace(static_cast<unsigned char>(c)))
-			{
-				++i;
-				continue;
-			}
-
-			// 2단계: 숫자가 시작되면 연속된 숫자 문자를 끝까지 모아 NUMBER 토큰 하나
-			if (std::isdigit(static_cast<unsigned char>(c)))
-			{
-				size_t start = i;
-				while (i < source.size() && std::isdigit(static_cast<unsigned char>(source[i])))
-					++i;
-				std::string origin = source.substr(start, i - start);
-				tokens.push_back(Token{ TokenType::NUMBER, origin, std::stod(origin) });
-				continue;
-			}
-
-			// 3단계: 알파벳: 연속된 영숫자를 모아 식별자, "var" 키워드인지 아닌지에 따라 VAR/IDENTIFIER 토큰으로 구분
-			if (std::isalpha(static_cast<unsigned char>(c)))
-			{
-				size_t start = i;
-				while (i < source.size() && std::isalnum(static_cast<unsigned char>(source[i])))
-					++i;
-				std::string origin = source.substr(start, i - start);
-				tokens.push_back(makeSimpleToken(origin == "var" ? TokenType::VAR : TokenType::IDENTIFIER, origin));
-				continue;
-			}
-
-			// 4단계: 나머지는 한 글자짜리 연산자/구두점 토큰으로 처리
-			if (auto symbol = scanSymbolToken(c))
-				tokens.push_back(*symbol);
+		// 1단계: 공백은 토큰 없이 SKIP
+		if (std::isspace(static_cast<unsigned char>(c)))
+		{
 			++i;
+			continue;
+		}
+
+		// 2단계: 숫자가 시작되면 연속된 숫자 문자를 끝까지 모아 NUMBER 토큰 하나
+		if (std::isdigit(static_cast<unsigned char>(c)))
+		{
+			size_t start = i;
+			while (i < source.size() && std::isdigit(static_cast<unsigned char>(source[i])))
+				++i;
+			std::string origin = source.substr(start, i - start);
+			tokens.push_back(Token{ TokenType::NUMBER, origin, std::stod(origin) });
+			continue;
+		}
+
+		// 3단계: 알파벳: 연속된 영숫자를 모아 식별자, "var" 키워드인지 아닌지에 따라 VAR/IDENTIFIER 토큰으로 구분
+		if (std::isalpha(static_cast<unsigned char>(c)))
+		{
+			size_t start = i;
+			while (i < source.size() && std::isalnum(static_cast<unsigned char>(source[i])))
+				++i;
+			std::string origin = source.substr(start, i - start);
+			TokenType type = TokenType::IDENTIFIER;
+			if (origin == "var")
+				type = TokenType::VAR;
+			tokens.push_back(makeSimpleToken(type, origin));
+			continue;
+		}
+
+		// 4단계: 나머지는 한 글자짜리 연산자/구두점 토큰으로 처리
+		if (auto symbol = scanSymbolToken(c))
+			tokens.push_back(*symbol);
+		++i;
 	}
 
 	// 5단계: 파서가 끝을 알 수 있도록 마지막에 END_OF_FILE 토큰 추가
