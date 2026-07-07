c4abbbb840086e8d74574c7a0a53fdf49b8bc358
JinyongPark <jin5383@naver.com>
2026-07-07 11:15:24 +0900

    [test] if 없음/if-else/dangling else 3개 TC를 하나로 병합
    if (true) { if (false) print "kfc"; else print "bbq"; } 시나리오 하나가
    outer if의 else 없음, inner if/else 양쪽 분기를 모두 검증하므로 별도였던
    IfWithoutElseBranch, IfElseBranchesBothPresent TC를 제거하고
    DanglingElseBindsToNearestIf 하나로 통합.

--- CodeFab_TeamD/test/test_Park.cpp
+++ CodeFab_TeamD/test/test_Park.cpp
@@ -386,71 +386,30 @@ TEST(AssemblerUnitTest, NestedBlockScopesReferenceEnclosingVariables)
 	EXPECT_EQ(right->name.origin, "inner");
 }
 
-// if (true) print "bbq"; : else 없는 if는 elseBranch가 nullptr이어야 한다
-TEST(AssemblerUnitTest, IfWithoutElseBranch)
-{
-	Program program = assemble("if (true) print \"bbq\";");
-
-	ASSERT_THAT(program.statements, SizeIs(1));
-	auto* ifStmt = dynamic_cast<IfStmt*>(statementAt(program.statements, 0));
-	ASSERT_THAT(ifStmt, NotNull());
-
-	auto* condition = dynamic_cast<LiteralExpr*>(ifStmt->condition);
-	ASSERT_THAT(condition, NotNull());
-	EXPECT_TRUE(std::get<bool>(condition->value));
-
-	auto* thenBranch = dynamic_cast<PrintStmt*>(ifStmt->thenBranch);
-	ASSERT_THAT(thenBranch, NotNull());
-	ASSERT_THAT(dynamic_cast<LiteralExpr*>(thenBranch->expression), NotNull());
-	EXPECT_EQ(stringValue(thenBranch->expression), "bbq");
-
-	EXPECT_THAT(ifStmt->elseBranch, IsNull());
-}
-
-// if (false) print "no"; else print "kfc"; : if/else 양쪽 분기가 모두 있어야 한다
-TEST(AssemblerUnitTest, IfElseBranchesBothPresent)
-{
-	Program program = assemble("if (false) print \"no\"; else print \"kfc\";");
-
-	ASSERT_THAT(program.statements, SizeIs(1));
-	auto* ifStmt = dynamic_cast<IfStmt*>(statementAt(program.statements, 0));
-	ASSERT_THAT(ifStmt, NotNull());
-
-	auto* condition = dynamic_cast<LiteralExpr*>(ifStmt->condition);
-	ASSERT_THAT(condition, NotNull());
-	EXPECT_FALSE(std::get<bool>(condition->value));
-
-	auto* thenBranch = dynamic_cast<PrintStmt*>(ifStmt->thenBranch);
-	ASSERT_THAT(thenBranch, NotNull());
-	ASSERT_THAT(dynamic_cast<LiteralExpr*>(thenBranch->expression), NotNull());
-	EXPECT_EQ(stringValue(thenBranch->expression), "no");
-
-	auto* elseBranch = dynamic_cast<PrintStmt*>(ifStmt->elseBranch);
-	ASSERT_THAT(elseBranch, NotNull());
-	ASSERT_THAT(dynamic_cast<LiteralExpr*>(elseBranch->expression), NotNull());
-	EXPECT_EQ(stringValue(elseBranch->expression), "kfc");
-}
-
-// 중첩 if에서 else는 가장 가까운 if(안쪽)에 결합되어야 한다 (dangling else)
+// else 는 가장 가까운 if 에 결합: if (true) { if (false) print "kfc"; else print "bbq"; }
 TEST(AssemblerUnitTest, DanglingElseBindsToNearestIf)
 {
-	Program program = assemble("if (true) if (false) print \"a\"; else print \"b\";");
+	Program program = assemble("if (true) { if (false) print \"kfc\"; else print \"bbq\"; }");
 
 	ASSERT_THAT(program.statements, SizeIs(1));
 	auto* outerIf = dynamic_cast<IfStmt*>(statementAt(program.statements, 0));
 	ASSERT_THAT(outerIf, NotNull());
 	EXPECT_THAT(outerIf->elseBranch, IsNull());
 
-	auto* innerIf = dynamic_cast<IfStmt*>(outerIf->thenBranch);
+	auto* outerBlock = dynamic_cast<BlockStmt*>(outerIf->thenBranch);
+	ASSERT_THAT(outerBlock, NotNull());
+	ASSERT_THAT(outerBlock->statements, SizeIs(1));
+
+	auto* innerIf = dynamic_cast<IfStmt*>(statementAt(outerBlock->statements, 0));
 	ASSERT_THAT(innerIf, NotNull());
 
 	auto* thenBranch = dynamic_cast<PrintStmt*>(innerIf->thenBranch);
 	ASSERT_THAT(thenBranch, NotNull());
 	ASSERT_THAT(dynamic_cast<LiteralExpr*>(thenBranch->expression), NotNull());
-	EXPECT_EQ(stringValue(thenBranch->expression), "a");
+	EXPECT_EQ(stringValue(thenBranch->expression), "kfc");
 
 	auto* elseBranch = dynamic_cast<PrintStmt*>(innerIf->elseBranch);
 	ASSERT_THAT(elseBranch, NotNull());
 	ASSERT_THAT(dynamic_cast<LiteralExpr*>(elseBranch->expression), NotNull());
-	EXPECT_EQ(stringValue(elseBranch->expression), "b");
+	EXPECT_EQ(stringValue(elseBranch->expression), "bbq");
 }
